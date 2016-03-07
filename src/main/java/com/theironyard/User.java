dd7afc5c38428a86decb487af37e76a24e00b568
oakes <zsoakes@gmail.com>
2016-03-07 12:05:57 -0500

    Initial import

---
+++ src/main/java/com/theironyard/User.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+/**
+ * Created by zach on 3/7/16.
+ */
+public class User {
+    public String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
